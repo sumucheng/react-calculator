c9a5a348669b09e3a8ea2c564eb72bbd5666087e
kiki <772765729@qq.com>
2019-11-27 22:37:24 +0800

    v1.0

---
+++ build/precache-manifest.70ab208b311190b68085e31b09ec1711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ce977b6a0faf9163acb0374a0b2734",
+    "url": "/react-calculator/build/index.html"
+  },
+  {
+    "revision": "a33727febd7e04bbdc42",
+    "url": "/react-calculator/build/static/css/main.6fb5f050.chunk.css"
+  },
+  {
+    "revision": "0e7b37dc915b8cf8c4ce",
+    "url": "/react-calculator/build/static/js/2.ef021b2d.chunk.js"
+  },
+  {
+    "revision": "a33727febd7e04bbdc42",
+    "url": "/react-calculator/build/static/js/main.d95ed09b.chunk.js"
+  },
+  {
+    "revision": "5e822126270733615722",
+    "url": "/react-calculator/build/static/js/runtime-main.921ec194.js"
+  }
+]);
